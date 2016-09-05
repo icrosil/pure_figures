82f94ec4c98dd14e693491790bf19c67b43f6a24
Illia Olenchenko <icrosil@gmail.com>
2016-09-05 14:13:22 +0300

    Added real work in c++

--- app/main.cu
+++ app/main.cu
@@ -1,12 +1,31 @@
 // Copyright 2016 Illia Olenchenko
 #include <iostream>
+#include <math.h>
+
+#include "./longStruct.cu"
 
 using namespace std;
 
+// N should be >=3
+const int N = 13;
+// L should be [4, 10, 52...]
+const int L = 4;
+
 __global__ void mykernel (void) {}
 
+int getNextMod(int MOD, int Mp) {
+  return (MOD * MOD - 2) % Mp;
+}
+
 int main(void) {
-  mykernel<<<1, 1>>>();
-  cout << "HELLO CUDA!" << endl;
+  // Calculating probable pure figure
+  int Mp = (int)pow(2.0, (double)N) - 1;
+  int MOD = L;
+  for (int i = 0; i < N - 2; ++i) {
+    MOD = getNextMod(MOD, Mp);
+    cout << MOD << endl;
+  }
+  bool isPure = MOD == 0;
+  cout << "is pure " << isPure << endl;
   return 0;
 }
